395d09671a3299592f0d21202eb6e2cc7ed56c3a
Keivy Mora <mkeivy.mora@gmail.com>
2023-03-30 17:29:15 -0400

    added handle error on uploadImg

--- utils/uploadImg.js
+++ utils/uploadImg.js
@@ -1,42 +1,46 @@
 import cloudinary from "cloudinary";
 
 const uploadImage = async (img) => {
-  let url = [];
+  try {
+    let url = [];
 
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
 
-  if (Array.isArray(img)) {
-    url = img.map(async (im) => {
-      let res = await cloudinary.uploader.upload(im?.tempFilePath, {
+    if (Array.isArray(img)) {
+      url = img.map(async (im) => {
+        let res = await cloudinary.uploader.upload(im?.tempFilePath, {
+          public_id: "123",
+        });
+        return cloudinary.url(res?.secure_url, {
+          width: 100,
+          height: 150,
+          Crop: "fill",
+        });
+      });
+
+      url = Promise.all(url);
+    } else {
+      let res = await cloudinary.uploader.upload(img?.tempFilePath, {
         public_id: "123",
       });
-      return cloudinary.url(res?.secure_url, {
-        width: 100,
-        height: 150,
-        Crop: "fill",
-      });
-    });
 
-    url = Promise.all(url);
-  } else {
-    let res = await cloudinary.uploader.upload(img?.tempFilePath, {
-      public_id: "123",
-    });
+      url.push(
+        cloudinary.url(res?.secure_url, {
+          width: 100,
+          height: 150,
+          Crop: "fill",
+        })
+      );
+    }
 
-    url.push(
-      cloudinary.url(res?.secure_url, {
-        width: 100,
-        height: 150,
-        Crop: "fill",
-      })
-    );
+    return url;
+  } catch (error) {
+    throw error?.message;
   }
-
-  return url;
 };
 
 export default uploadImage;
